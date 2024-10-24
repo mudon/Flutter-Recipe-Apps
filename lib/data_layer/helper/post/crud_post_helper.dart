5ae0b725173223b02497ff715ce26f06f2c7bbfe
mudon <muhammadhazim57@gmail.com>
2024-10-24 17:44:55 +0800

    new

--- lib/data_layer/helper/post/crud_post_helper.dart
+++ lib/data_layer/helper/post/crud_post_helper.dart
@@ -1,11 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:recipe_project/data_layer/repo/posts/crud_post.dart';
+import 'package:recipe_project/data_layer/services/auth_service.dart';
+import 'package:uuid/uuid.dart';
 
 class CrudPostHelper {
   static Future<void> addPostFromBackend() async {
     await CrudPost.addPostFromBackend();
   }
 
-  static Future<void> addPost() async {}
+  static Future<void> addPost(dynamic recipeData) async {
+    const uuid = Uuid();
+    String postUid = uuid.v4();
+
+    Map<String, List<String>> reTypeBahan =
+        (recipeData["bahan"] as Map<String, dynamic>).map(
+      (key, value) => MapEntry(key, List<String>.from(value as List<dynamic>)),
+    );
+
+    Map<String, List<String>> reTypePenyediaan =
+        (recipeData["penyediaan"] as Map<String, dynamic>).map(
+      (key, value) => MapEntry(key, List<String>.from(value as List<dynamic>)),
+    );
+
+    Map<String, Map<String, dynamic>>? likesData = (recipeData["likes"] != null)
+        ? (recipeData["likes"] as Map<dynamic, dynamic>).map(
+            (key, value) => MapEntry(
+              key as String,
+              Map<String, dynamic>.from(value as Map),
+            ),
+          )
+        : {};
+
+    List<Map<String, dynamic>>? commentsData = (recipeData["comments"] != null)
+        ? List<Map<String, dynamic>>.from(
+            recipeData["comments"] as List<dynamic>)
+        : [];
+
+    Map<String, dynamic> postData = {
+      "id": postUid,
+      "authorId": AuthService.user?.uid,
+      "bahan": reTypeBahan,
+      "penyediaan": reTypePenyediaan,
+      "contentTitle": recipeData["name"],
+      "contentImg":
+          "https://firebasestorage.googleapis.com/v0/b/bahtera-resipi-d733f.appspot.com/o/recipeImg%2F${recipeData["name"].toLowerCase().replaceAll(' ', '-')}.jpg?alt=media&token=963031fd-28c1-417b-aeb8-33c9017dcb03",
+      "contentDescription": "dummyContent",
+      "timePosted": Timestamp.now(),
+      "likes": likesData,
+      "comments": commentsData,
+    };
+
+    CrudPost.addPost(postData);
+  }
 
   static Future<void> editPost() async {}
   static Future<void> removePost() async {}
--- lib/data_layer/repo/posts/crud_post.dart
+++ lib/data_layer/repo/posts/crud_post.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:recipe_project/data_layer/helper/post/crud_post_helper.dart';
 import 'package:recipe_project/data_layer/models/post.dart';
 import 'package:recipe_project/data_layer/repo/recipe_list_repo.dart';
 import 'package:recipe_project/data_layer/repo/utils/direct_firebase.dart';
@@ -12,59 +13,18 @@ class CrudPost {
 
     if (recipeList.isNotEmpty) {
       for (dynamic recipe in recipeList) {
-        CrudPost.addPost(recipe);
+        CrudPostHelper.addPost(recipe);
       }
     }
   }
 
-  static Future<void> addPost(dynamic recipeData) async {
-    const uuid = Uuid();
-    String postUid = uuid.v4();
-
-    Map<String, List<String>> reTypeBahan =
-        (recipeData["bahan"] as Map<String, dynamic>).map(
-      (key, value) => MapEntry(key, List<String>.from(value as List<dynamic>)),
-    );
-
-    Map<String, List<String>> reTypePenyediaan =
-        (recipeData["penyediaan"] as Map<String, dynamic>).map(
-      (key, value) => MapEntry(key, List<String>.from(value as List<dynamic>)),
-    );
-
-    Map<String, Map<String, dynamic>>? likesData = (recipeData["likes"] != null)
-        ? (recipeData["likes"] as Map<dynamic, dynamic>).map(
-            (key, value) => MapEntry(
-              key as String,
-              Map<String, dynamic>.from(value as Map),
-            ),
-          )
-        : {};
-
-    List<Map<String, dynamic>>? commentsData = (recipeData["comments"] != null)
-        ? List<Map<String, dynamic>>.from(
-            recipeData["comments"] as List<dynamic>)
-        : [];
-
-    Map<String, dynamic> postData = {
-      "id": postUid,
-      "authorId": AuthService.user?.uid,
-      "bahan": reTypeBahan,
-      "penyediaan": reTypePenyediaan,
-      "contentTitle": recipeData["name"],
-      "contentImg":
-          "https://firebasestorage.googleapis.com/v0/b/bahtera-resipi-d733f.appspot.com/o/recipeImg%2Fnasi-ayam.jpg?alt=media&token=963031fd-28c1-417b-aeb8-33c9017dcb03",
-      "contentDescription": "dummyContent",
-      "timePosted": Timestamp.now(),
-      "likes": likesData,
-      "comments": commentsData,
-    };
-
+  static Future<void> addPost(Map<String, dynamic> postData) async {
     await DirectFirebase.firestoreDatabase
         .collection("posts")
-        .doc(postUid)
+        .doc(postData["id"])
         .set(postData);
 
-    await PostModel.fromMap(postData);
+    PostModel.fromMap(postData);
   }
 
   static Future<void> removePost() async {}
